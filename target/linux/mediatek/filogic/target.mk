06e559be0f5616c48e79d60abef3cdd3bd186133
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-06-25 11:23:10 +0200

    Update DEFAULT_PACKAGES in target.mk for Filogic
    Expanded the DEFAULT_PACKAGES list for the Filogic target.

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -2,7 +2,7 @@ ARCH:=aarch64
 SUBTARGET:=filogic
 BOARDNAME:=Filogic 8x0 (MT798x)
 CPU_TYPE:=cortex-a53
-DEFAULT_PACKAGES += fitblk kmod-crypto-hw-safexcel wpad-basic-mbedtls uboot-envtools
+DEFAULT_PACKAGES += fitblk kmod-crypto-hw-safexcel wpad-basic-mbedtls uboot-envtools apk-mbedtls base-files ca-bundle dnsmasq dropbear firewall4 fitblk fstools kmod-crypto-hw-safexcel kmod-gpio-button-hotplug kmod-leds-gpio kmod-nft-offload libc libgcc libustream-mbedtls logd mtd netifd nftables odhcp6c odhcpd-ipv6only ppp ppp-mod-pppoe procd-ujail uboot-envtools uci uclient-fetch urandom-seed urngd wpad-basic-mbedtls kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware luci
 KERNELNAME:=Image dtbs
 DEFAULT_PROFILE:=openwrt_one
 
